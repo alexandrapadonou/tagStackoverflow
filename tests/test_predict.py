094a540fa45a5eab19fa5893f0aec657c7647229
apadonou <alexandra-epiphanie.padonou@etudiant.univ-rennes1.fr>
2025-12-29 11:26:13 +0100

    solving test errors

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -1,12 +1,12 @@
+# tests/test_predict.py
 from fastapi.testclient import TestClient
 from api.main import app
 
-client = TestClient(app)
-
 def test_predict_response():
-    payload = {"text": "Hello world"}
-    res = client.post("/predict", json=payload)
-    assert res.status_code == 200
-    # data = res.json()
-    # assert "tags" in data
-    # assert isinstance(data["tags"], list)
+    with TestClient(app) as client:  # important : déclenche startup/shutdown
+        payload = {"text": "Hello world"}
+        res = client.post("/predict", json=payload)
+        assert res.status_code == 200
+        # data = res.json()
+        # assert "tags" in data
+        # assert isinstance(data["tags"], list)
